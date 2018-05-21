b10c35ec6509018c7bba6b1fbef7b76047634fe0
Subhajit Sahu <wolfram77@gmail.com>
2018-05-21 21:58:44 +0530

    :snowman: comments in csv

--- gen.js
+++
@@ -1,8 +0,0 @@
-const columns = require('./columns');
-const fs = require('fs');
-const os = require('os');
-
-var z = `code,actual${os.EOL}`;
-for(var [k, v] of columns)
-  z += `${k},${v}${os.EOL}`;
-fs.writeFileSync('columns.csv', z);
