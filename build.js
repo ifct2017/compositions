f62e6730502fca23dcabff9c040966a73f260c34
Subhajit Sahu <wolfram77@gmail.com>
2018-07-03 15:02:47 +0530

    :snowman: use aggregate from aggregate.csv

--- build.js
+++ build.js
@@ -95,78 +95,18 @@ function nullToZero(d) {
   }
 };
 
-function combinedColumns(d) {
-  d.vitb = []; d.vitb_e = []; d.vitd = []; d.vitd_e = []; d.tocph = []; d.tocph_e = [];
-  d.toctr = []; d.toctr_e = []; d.vitk = []; d.vitk_e = [];
-  d.amiace = []; d.amiace_e = []; d.amiacce = []; d.amiacce_e = []; d.amiacne = []; d.amiacne_e = [];
-  d.amiac = []; d.amiac_e = []; d.orgac = []; d.orgac_e = [];
-  d.fauns = []; d.fauns_e = []; d.faess = []; d.faess_e = []; d.facn3 = []; d.facn3_e = [];
-  d.facn6 = []; d.facn6_e = []; d.facn9 = []; d.facn9_e = []; d.famscis = []; d.famscis_e = [];
-  d.fatrn = []; d.fatrn_e = [];
-  d.olsac = []; d.olsac_e = []; d.phystr = []; d.phystr_e = []; d.mnrleq = []; d.mnrleq_e = [];
-  d.mnrlet = []; d.mnrlet_e = []; d.mnrlpet = []; d.mnrlpet_e = []; d.mnrlnet = []; d.mnrlnet_e = [];
-  d.mnrltx = []; d.mnrltx_e = [];
-  d.carot = []; d.carot_e = []; d.xantp = []; d.xantp_e = []; d.cartbeq = []; d.cartbeq_e = [];
-  d.vita = []; d.vita_e = []; d.vit = []; d.vit_e = [];
-  for(var i=0; i<di; i++) {
-    d.vitb[i] = round(d.thia[i]+d.ribf[i]+d.nia[i]+d.pantac[i]+d.vitb6c[i]+d.biot[i]+d.folsum[i]);
-    d.vitb_e[i] = round(d.thia_e[i]+d.ribf_e[i]+d.nia_e[i]+d.pantac_e[i]+d.vitb6c_e[i]+d.biot_e[i]+d.folsum_e[i]);
-    d.vitd[i] = round(d.ergcal[i]+d.chocal[i]+d.doh25[i]);
-    d.vitd_e[i] = round(d.ergcal_e[i]+d.chocal_e[i]+d.doh25_e[i]);
-    d.tocph[i] = round(d.tocpha[i]+d.tocphb[i]+d.tocphg[i]+d.tocphd[i]);
-    d.tocph_e[i] = round(d.tocpha_e[i]+d.tocphb_e[i]+d.tocphg_e[i]+d.tocphd_e[i]);
-    d.toctr[i] = round(d.toctra[i]+d.toctrb[i]+d.toctrg[i]+d.toctrd[i]);
-    d.toctr_e[i] = round(d.toctra_e[i]+d.toctrb_e[i]+d.toctrg_e[i]+d.toctrd_e[i]);
-    d.vitk[i] = round(d.vitk1[i]+d.vitk2[i]);
-    d.vitk_e[i] = round(d.vitk1_e[i]+d.vitk2_e[i]);
-    d.amiace[i] = round(d.his[i]+d.ile[i]+d.leu[i]+d.lys[i]+d.met[i]+d.phe[i]+d.thr[i]+d.trp[i]+d.val[i]);
-    d.amiace_e[i] = round(d.his_e[i]+d.ile_e[i]+d.leu_e[i]+d.lys_e[i]+d.met_e[i]+d.phe_e[i]+d.thr_e[i]+d.trp_e[i]+d.val_e[i]);
-    d.amiacce[i] = round(d.arg[i]+d.cys[i]+d.gly[i]+d.pro[i]+d.tyr[i]);
-    d.amiacce_e[i] = round(d.arg_e[i]+d.cys_e[i]+d.gly_e[i]+d.pro_e[i]+d.tyr_e[i]);
-    d.amiacne[i] = round(d.ala[i]+d.asp[i]+d.glu[i]+d.ser[i]);
-    d.amiacne_e[i] = round(d.ala_e[i]+d.asp_e[i]+d.glu_e[i]+d.ser_e[i]);
-    d.amiac[i] = round(d.amiace[i]+d.amiacce[i]+d.amiacne[i]);
-    d.amiac_e[i] = round(d.amiace_e[i]+d.amiacce_e[i]+d.amiacne_e[i]);
-    d.orgac[i] = round(d.oxalt[i]+d.caconac[i]+d.citac[i]+d.fumac[i]+d.malac[i]+d.quinac[i]+d.sucac[i]+d.tarac[i]);
-    d.orgac_e[i] = round(d.oxalt_e[i]+d.caconac_e[i]+d.citac_e[i]+d.fumac_e[i]+d.malac_e[i]+d.quinac_e[i]+d.sucac_e[i]+d.tarac_e[i]);
-    d.fauns[i] = round(d.fams[i]+d.fapu[i]);
-    d.fauns_e[i] = round(d.fams_e[i]+d.fapu_e[i]);
-    d.faess[i] = round(d.f18d3n3[i]+d.f18d2cn6[i]+d.f20d5n3[i]+d.f22d6n3[i]+d.f20d4n6[i]);
-    d.faess_e[i] = round(d.f18d3n3_e[i]+d.f18d2cn6_e[i]+d.f20d5n3_e[i]+d.f22d6n3_e[i]+d.f20d4n6_e[i]);
-    d.facn3[i] = round(d.f18d3n3[i]+d.f20d5n3[i]+d.f22d5n3[i]+d.f22d6n3[i]);
-    d.facn3_e[i] = round(d.f18d3n3_e[i]+d.f20d5n3_e[i]+d.f22d5n3_e[i]+d.f22d6n3_e[i]);
-    d.facn6[i] = round(d.f18d2cn6[i]+d.f20d2n6[i]+d.f22d2n6[i]+d.f20d3n6[i]+d.f20d4n6[i]);
-    d.facn6_e[i] = round(d.f18d2cn6_e[i]+d.f20d2n6_e[i]+d.f22d2n6_e[i]+d.f20d3n6_e[i]+d.f20d4n6_e[i]);
-    d.facn9[i] = round(d.f18d1cn9[i]+d.f20d1cn9[i]+d.f22d1cn9[i]+d.f24d1cn9[i]);
-    d.facn9_e[i] = round(d.f18d1cn9_e[i]+d.f20d1cn9_e[i]+d.f22d1cn9_e[i]+d.f24d1cn9_e[i]);
-    d.famscis[i] = round(d.f14d1cn5[i]+d.f16d1cn7[i]+d.f18d1cn9[i]+d.f20d1cn9[i]+d.f22d1cn9[i]+d.f24d1cn9[i]);
-    d.famscis_e[i] = round(d.f14d1cn5_e[i]+d.f16d1cn7_e[i]+d.f18d1cn9_e[i]+d.f20d1cn9_e[i]+d.f22d1cn9_e[i]+d.f24d1cn9_e[i]);
-    d.fatrn[i] = round(d.f18d1tn9[i]);
-    d.fatrn_e[i] = round(d.f18d1tn9_e[i]);
-    d.olsac[i] = round(d.rafs[i]+d.stas[i]+d.vers[i]+d.ajgs[i]);
-    d.olsac_e[i] = round(d.rafs_e[i]+d.stas_e[i]+d.vers_e[i]+d.ajgs_e[i]);
-    d.phystr[i] = round(d.camt[i]+d.stgstr[i]+d.stostrb[i]);
-    d.phystr_e[i] = round(d.camt_e[i]+d.stgstr_e[i]+d.stostrb_e[i]);
-    d.mnrleq[i] = round(d.na[i]+d.mg[i]+d.k[i]+d.ca[i]+d.p[i]);
-    d.mnrleq_e[i] = round(d.na_e[i]+d.mg_e[i]+d.k_e[i]+d.ca_e[i]+d.p_e[i]);
-    d.mnrlet[i] = round(d.cr[i]+d.mo[i]+d.mn[i]+d.fe[i]+d.co[i]+d.ni[i]+d.cu[i]+d.zn[i]+d.as[i]+d.se[i]);
-    d.mnrlet_e[i] = round(d.cr_e[i]+d.mo_e[i]+d.mn_e[i]+d.fe_e[i]+d.co_e[i]+d.ni_e[i]+d.cu_e[i]+d.zn_e[i]+d.as_e[i]+d.se_e[i]);
-    d.mnrlpet[i] = round(d.li[i]);
-    d.mnrlpet_e[i] = round(d.li_e[i]);
-    d.mnrlnet[i] = round(d.al[i]+d.cd[i]+d.pb[i]);
-    d.mnrlnet_e[i] = round(d.al_e[i]+d.cd_e[i]+d.pb_e[i]);
-    d.mnrltx[i] = round(d.hg[i]);
-    d.mnrltx_e[i] = round(d.hg_e[i]);
-    d.carot[i] = round(d.lycpn[i]+d.cartg[i]+d.carta[i]+d.cartb[i]);
-    d.carot_e[i] = round(d.lycpn_e[i]+d.cartg_e[i]+d.carta_e[i]+d.cartb_e[i]);
-    d.xantp[i] = round(d.lutn[i]+d.zea[i]+d.crypxb[i]);
-    d.xantp_e[i] = round(d.lutn_e[i]+d.zea_e[i]+d.crypxb_e[i]);
-    d.cartbeq[i] = round(d.carta[i]+d.cartb[i]+d.cartg[i]+d.crypxb[i]);
-    d.cartbeq_e[i] = round(d.carta_e[i]+d.cartb_e[i]+d.cartg_e[i]+d.crypxb_e[i]);
-    d.vita[i] = round(d.retol[i]+d.cartbeq[i]);
-    d.vita_e[i] = round(d.retol_e[i]+d.cartbeq_e[i]);
-    d.vit[i] = round(d.vita[i]+d.vitb[i]+d.vitc[i]+d.vitd[i]+d.vite[i]+d.vitk[i]);
-    d.vit_e[i] = round(d.vita_e[i]+d.vitb_e[i]+d.vitc_e[i]+d.vitd_e[i]+d.vite_e[i]+d.vitk_e[i]);
+function sumColumns(d, i, ks) {
+  var z = 0;
+  for(var k of ks)
+    z += d[k][i];
+  return z;
+};
+
+function aggregateAll(d) {
+  for(var [k, exp] of aggregate) {
+    var sumk = exp.replace(/\s/g, '').split('+'); d[k] = [];
+    for(var i=0; i<di; i++)
+      d[k][i] = round(sumColumns(d, i, sumk));
   }
 };
 
@@ -178,7 +118,7 @@ async function build() {
   for(var file of fs.readdirSync('assets'))
     await readAsset(path.join('assets', file));
   nullToZero(dat);
-  combinedColumns(dat);
+  aggregateAll(dat);
   var ks = Object.keys(dat), z = ks.join()+os.EOL;
   for(var i=0; i<di; i++) {
     for(var k of ks) {
